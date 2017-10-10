d32aea632f13f677f38a86ebf1909e81c2ac9b8a
shen-sat <shensat18@gmail.com>
2017-10-10 17:53:18 +0100

    put in test var

--- twitter_app_controller.rb
+++ twitter_app_controller.rb
@@ -18,10 +18,11 @@ class Begin
 	def init_app
 		#Twitter authentication stuff **--REMEMBER TO DELETE YOUR DETAILS IF YOU MAKE PUBLIC--**
 		client = Twitter::REST::Client.new do |config|
-		  config.consumer_key        = 'insert your key'
-		  config.consumer_secret     = 'insert your key'
-		  config.access_token        = 'insert your key'
-		  config.access_token_secret = 'insert your key'
+		  config.consumer_key        = ENV['CONSUMER_KEY']
+		  config.consumer_secret     = ENV['CONSUMER_SECRET']
+		  config.access_token        = ENV['ACCESS_TOKEN']
+		  config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+		  @test_var = "hello world"
 		 end
 
 		#get the time limit for 24 hours before the time of search and convert it to string 
@@ -59,7 +60,7 @@ class Begin
 		#set up the html code for the page and insert pixel art images and assign it to variable 'template'
 		template = %(
 		<html>
-		<title>W3.CSS Template</title>
+		<title><%= @test_var %></title>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
